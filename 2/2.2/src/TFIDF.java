c15f1d1bfc343696b379c90cf957799384c67603
architsangal <architsangal2000@gmail.com>
2023-03-29 01:29:54 +0530

    added cammand

--- 2/2.2/src/TFIDF.java
+++ 2/2.2/src/TFIDF.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Writable;
@@ -25,6 +26,62 @@ import opennlp.tools.stemmer.PorterStemmer;
 
 public class TFIDF
 {
+    public static class MapTF extends Mapper<Text, Text, Text, MapWritable>
+    {
+        @Override
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException
+        {
+
+        	String line = value.toString();
+        	String[] tokens = line.split("[^\\w']+");
+        	        	
+        	PorterStemmer steammer = new PorterStemmer();
+        	for (int i = 0; i < tokens.length; i++)
+        		tokens[i] = steammer.stem(tokens[i]).toString();
+        	int[] freq = new int[100];
+        	Top obj = new Top();
+        	String[] top = obj.top;
+        	for(String token : tokens)
+        	{
+        		for(int i=0;i<100;i++)
+        		{
+        			if(token.equals(top[i]))
+        			{
+        				freq[i]++;
+        			}
+        		}
+        	}
+        	
+            MapWritable map = new MapWritable();
+        	for(int i=0;i<100;i++)
+        	{
+        		map.put(new Text(top[i]),new LongWritable(freq[i]));
+        	}
+        	
+        	context.write(new Text(key.toString()), map);
+        }
+    }
+
+    public static class ReduceTF extends Reducer<Text, MapWritable, Text, Text>
+    {
+
+        @Override
+        public void reduce(Text key, Iterable<MapWritable> values, Context context) throws IOException, InterruptedException
+        {
+
+            for (MapWritable value : values)
+            {
+                for(MapWritable.Entry<Writable, Writable> e : value.entrySet())
+                {
+                	System.out.println(key.toString());
+                    context.write(new Text(key.toString()+"\t"+e.getKey().toString()),new Text(e.getValue().toString()));
+                }
+
+            }
+        }
+    }
+
+
     public static class Map extends Mapper<Object, Text, Text, Text>
     {
         @Override
@@ -65,22 +122,45 @@ public class TFIDF
  
     public static void main(String[] args) throws Exception
     {
-    	    	
-        Configuration conf = new Configuration();		
-		Job job = Job.getInstance(conf, "Stripes");
+        Configuration conf = new Configuration();
+		
+		Job job0 = Job.getInstance(conf, "MR TF");
+        //job.setJarByClass(TF.class);
+
+        //job.addCacheFile(new URI("/Users/architsangal/Data/College/Semester/NoSQL/Assignment/Assignment_2/Hadoop-NLP/TarFiles/output/full/part-r-00000"));
         
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
+        job0.setInputFormatClass(DFInputFormat.class);
+        
+        job0.setMapperClass(MapTF.class);
+        job0.setReducerClass(ReduceTF.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        job0.setOutputKeyClass(Text.class);
+        job0.setOutputValueClass(MapWritable.class);
 
-        FileInputFormat.setInputPaths(job, new Path(args[2]));
-        FileOutputFormat.setOutputPath(job, new Path(args[3]));
+        job0.setOutputFormatClass(TextOutputFormat.class);
 
-        job.waitForCompletion(true);
+        FileInputFormat.setInputPaths(job0, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job0, new Path(args[1]));
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+    	///////////////////
+        Configuration conf1 = new Configuration();		
+		Job job1 = Job.getInstance(conf1, "Stripes");
+        
+        job1.setMapperClass(Map.class);
+        job1.setReducerClass(Reduce.class);
+
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(Text.class);
+
+        FileInputFormat.setInputPaths(job1, new Path(args[2]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[3]));
+
+		///////////////////
+
+        if(job0.waitForCompletion(true))
+        	System.exit(job1.waitForCompletion(true) ? 0 : 1);
+		
+		
 	}
     
     public static void run(String[] args)throws IOException
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ $ rm -r output
 $ hadoop jar temp.jar DF ./../Wikipedia-articles/test/ ./output/test
 $ hadoop jar temp.jar DF ./../Wikipedia-articles/full/ ./output/full
 $ java top.java < ./../output/full/part-r-00000
+$ hadoop jar temp_2.jar TFIDF ./../Wikipedia-articles/test/ ./output/secondtest/ ./../TarFiles/output/secondtest/part-r-00000  ./output/secondsectest
 ```
